c26075311144e8af4ff209bf028e95cd572b73aa
alexiaaw <142686806+alexiaaw@users.noreply.github.com>
2024-02-27 21:02:56 -0600

    test.bat

---
+++ Hans-on 3/helloworld/test.bat
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Imprime texto en pantalla
+echo "Hello World! :)"
+
+# Lista el contenido del directorio
+ls
+
+# Crea un directorio 
+mkdir Test
+
+# Cambia al directorio T
+cd Test
+
+# Lista el contenido del directorio 
+ls
