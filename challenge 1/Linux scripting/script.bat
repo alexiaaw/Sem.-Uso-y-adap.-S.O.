55dd31a5a1b09b10d3b48336aa67cd8dd8d7f636
alexiaaw <142686806+alexiaaw@users.noreply.github.com>
2024-03-01 22:44:11 -0600

    script.bat

--- challenge 1/Linux scripting/script.bat
+++ challenge 1/Linux scripting/script.bat
@@ -1,31 +1,32 @@
 #!/bin/bash
 
-# Verificar que se hayan proporcionado todos los argumentos
-if [ $# -ne 6 ]; then
-    echo "Uso: $0 <Nombre_VM> <Tipo_SO> <CPUs> <Memoria_GB> <VRAM_MB> <Tamaño_Disco_GB>"
+# Verificar el número correcto de argumentos
+if [ "$#" -ne 6 ]; then
+    echo "Uso: $0 <Nombre_VM> <CPUs> <Memoria_GB> <VRAM_MB> <Tamaño_Disco_GB>"
     exit 1
 fi
 
-# Asignar los argumentos a variables
-nombre_vm=$1
-tipo_so=$2
-cpus=$3
-memoria_gb=$4
-vram_mb=$5
-tamano_disco_gb=$6
+# Obtener los argumentos
+NOMBRE_VM="$1"
+CPUS="$2"
+MEMORIA_GB="$3"
+VRAM_MB="$4"
+TAMAÑO_DISCO_GB="$5"
 
 # Crear la máquina virtual
-VBoxManage createvm --name "$nombre_vm" --ostype "$tipo_so" --register
+VBoxManage createvm --name "$NOMBRE_VM" --register
 
-# Configurar los componentes de la máquina virtual
-VBoxManage modifyvm "$nombre_vm" --cpus "$cpus"
-VBoxManage modifyvm "$nombre_vm" --memory "$memoria_gb"GB
-VBoxManage modifyvm "$nombre_vm" --vram "$vram_mb"
-VBoxManage createmedium disk --filename "$nombre_vm.vdi" --size "$tamano_disco_gb"GB --format VDI
-VBoxManage storagectl "$nombre_vm" --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage storageattach "$nombre_vm" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$nombre_vm.vdi"
-VBoxManage storagectl "$nombre_vm" --name "IDE Controller" --add ide --controller PIIX4
+# Configurar la máquina virtual
+VBoxManage modifyvm "$NOMBRE_VM" --cpus "$CPUS" --memory "$MEMORIA_GB" --vram "$VRAM_MB"
 
-# Imprimir la configuración de la máquina virtual
-echo "Configuración de la máquina virtual $nombre_vm:"
-VBoxManage showvminfo "$nombre_vm"
+# Crear y adjuntar el disco duro virtual
+VBoxManage createmedium disk --filename "$NOMBRE_VM.vdi" --size "$TAMAÑO_DISCO_GB"
+
+VBoxManage storagectl "$NOMBRE_VM" --name "SATA Controller" --add sata --controller IntelAhci
+VBoxManage storageattach "$NOMBRE_VM" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$NOMBRE_VM.vdi"
+
+# Mostrar la configuración de la máquina virtual
+VBoxManage showvminfo "$NOMBRE_VM"
+
+echo "Máquina virtual '$NOMBRE_VM' creada y configurada correctamente."
+
